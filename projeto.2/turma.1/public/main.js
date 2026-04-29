dd6aa16f666b8f1cec7d2a143c7cc65b675b4618
niel <niel@localhost>
2026-04-29 17:07:06 -0300

    ...

--- projeto.2/turma.1/public/main.js
+++ projeto.2/turma.1/public/main.js
@@ -8,12 +8,19 @@ function criarTarefa(texto) {
   if (texto.trim() === '') return;
   const tarefa = templateTarefa.content.cloneNode(true);
   const spanTitle = tarefa.querySelector('span');
+  const btnExcluir = tarefa.querySelector('button');
   spanTitle.textContent = texto;
   containerTarefas.appendChild(tarefa);
+  // não feito aula passada
+  btnExcluir.onclick = function () {
+    this.closest('.tarefa').remove();
+  }
 }
 
 // btnAdicionar.addEventListener('click', () => {})
-btnAdicionar.onclick = function() {
+btnAdicionar.onclick = function () {
   const texto = inputTarefa.value.trim();
   criarTarefa(texto);
+  // não feito aula passada
+  inputTarefa.value = '';
 }
