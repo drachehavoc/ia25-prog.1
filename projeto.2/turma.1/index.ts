5c4580699f4dd26740cba58e5fef3eb0cbd716a1
niel <niel@localhost>
2026-05-06 16:53:38 -0300

    criado servidor de arquivos simples

--- projeto.2/turma.1/index.ts
+++ projeto.2/turma.1/index.ts
@@ -1,20 +1,16 @@
-import { file } from "bun";
-
 const server = Bun.serve({
   routes: {},
-
   async fetch(req) {
     const url = new URL(req.url);
     const pathname = url.pathname;
-    const filePath = pathname === "/" 
-      ? "./public/index.html" 
+    const localPath = (pathname === "/") 
+      ? `./public/index.html`
       : `./public${pathname}`;
-    const file = Bun.file(filePath);
+    const file = Bun.file(localPath);
     const exists = await file.exists();
-    if (!exists) 
+    if (!exists)
       return new Response("Not Found", { status: 404 });
     return new Response(file);
   },
 });
-
 console.log(`Server running at ${server.url}`);
